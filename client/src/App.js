e5dabd776643f37bf870453d0272cfae72d45682
Apna Anuj <121931935+ApnaAnuj@users.noreply.github.com>
2023-08-28 19:38:11 +0530

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,175 +1,175 @@
+// Importing necessary dependencies
 import logo from "./logo.svg";
 import "./App.css";
-import {MdClose} from "react-icons/md"
-import { useEffect,useState } from 'react';
-import axios from "axios"
-import Formtable from './components/Formtable';
+import { MdClose } from "react-icons/md";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Formtable from "./components/Formtable";
 
-axios.defaults.baseURL = "http://localhost:8080/"
+// Setting the default base URL for axios requests
+axios.defaults.baseURL = "http://localhost:8080/";
 
+// Main App component
 function App() {
+  // State variables for managing add and edit sections
+  const [addSection, setAddSection] = useState(false);
+  const [editSection, setEditSection] = useState(false);
 
-  const [addSection , setAddSection ] = useState(false)
-  const [editSection,setEditSection] = useState(false)
+  // State variables for form data and editing data
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+  });
+  const [formDataEdit, setFormDataEdit] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+    _id: "",
+  });
 
-  const [formData,setFormData] = useState({
-    name : "",
-    email : "",
-    mobile : "",
-  })
-  const [formDataEdit,setFormDataEdit] = useState({
-    name : "",
-    email : "",
-    mobile : "",
-    _id : ""
-  })
-const [dataList,setDataList] = useState([])
+  // State variable to store fetched data
+  const [dataList, setDataList] = useState([]);
 
-//..........................................................................
+  // Function to handle form input changes
+  const handleOnChange = (e) => {
+    const { value, name } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-  const handleOnChange = (e)=>{
-    const {value,name} = e.target
-    setFormData((preve)=>{
-        return{
-          ...preve,
-          [name] : value
-        }
-    })
-  }
-
-//..........................................................................
-
-  const handleSubmit = async(e)=>{
-    e.preventDefault()
-    const data = await axios.post("/create",formData)
-    console.log(data)
-    if(data.data.success){
-        setAddSection(false)
-        alert(data.data.message)
-        getFetchData()
-        setFormData({
-          name : "",
-          email : "",
-          mobile : ""
-        })
+  // Function to handle form submission for adding data
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = await axios.post("/create", formData);
+    if (data.data.success) {
+      setAddSection(false);
+      alert(data.data.message);
+      getFetchData();
+      setFormData({
+        name: "",
+        email: "",
+        mobile: "",
+      });
     }
-  }
-//................................................................................
-  const getFetchData = async()=>{
-    const data = await axios.get("/")
-    console.log(data)
-    if(data.data.success){
-        setDataList(data.data.data)
+  };
+
+  // Function to fetch data from the server
+  const getFetchData = async () => {
+    const data = await axios.get("/");
+    if (data.data.success) {
+      setDataList(data.data.data);
     }
-  }
-  useEffect(()=>{
-    getFetchData()
-  },[])
+  };
 
-  //...........................................................................
-  const handleDelete = async(id)=>{
-    const data = await axios.delete("/delete/"+id)
-    
-      if(data.data.success){
-        getFetchData()
-        alert(data.data.message)
-      }
-  }
-  //..............................................................................
-  const handleUpdate = async(e)=>{
-    e.preventDefault()
-    const data = await axios.put("/update",formDataEdit)
-    if(data.data.success){
-      getFetchData()
-      alert(data.data.message)
-      setEditSection(false)
+  // Fetch initial data on component mount
+  useEffect(() => {
+    getFetchData();
+  }, []);
 
+  // Function to handle data deletion
+  const handleDelete = async (id) => {
+    const data = await axios.delete("/delete/" + id);
+    if (data.data.success) {
+      getFetchData();
+      alert(data.data.message);
     }
-  }
-  
-//..........................................................................
-  const handleEditOnChange = async(e)=>{
-    const {value,name} = e.target
-    setFormDataEdit((preve)=>{
-        return{
-          ...preve,
-          [name] : value
-        }
-    })
-  }
-  //..........................................................................
-  const handleEdit = (el)=>{
-    setFormDataEdit(el)
-    setEditSection(true)
-  }
+  };
 
-//...............................................................................................
+  // Function to handle form submission for updating data
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const data = await axios.put("/update", formDataEdit);
+    if (data.data.success) {
+      getFetchData();
+      alert(data.data.message);
+      setEditSection(false);
+    }
+  };
+
+  // Function to handle input changes in edit form
+  const handleEditOnChange = (e) => {
+    const { value, name } = e.target;
+    setFormDataEdit((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  // Function to handle editing data
+  const handleEdit = (el) => {
+    setFormDataEdit(el);
+    setEditSection(true);
+  };
+
+  // JSX for rendering the component
   return (
     <>
       <div className="container">
-        <button className="btn  btn-add" onClick={()=>setAddSection(true)}> Add</button>
+        <button className="btn btn-add" onClick={() => setAddSection(true)}>
+          Add
+        </button>
 
-        {
-          addSection &&(
-            <Formtable
+        {addSection && (
+          <Formtable
             handleSubmit={handleSubmit}
             handleOnChange={handleOnChange}
-            handleclose = {()=>setAddSection(false)}
+            handleclose={() => setAddSection(false)}
             rest={formData}
           />
-          )
-        }
-        {
-          editSection && (
+        )}
+
+        {editSection && (
           <Formtable
             handleSubmit={handleUpdate}
             handleOnChange={handleEditOnChange}
-            handleclose = {()=>setEditSection(false)}
+            handleclose={() => setEditSection(false)}
             rest={formDataEdit}
           />
-        )
-        }
+        )}
 
-
-        <div className='tableContainer'>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Mobile</th>
-              <th>
-
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            { 
-              dataList[0] ? (
-              dataList.map((el)=>{
-                console.log(el)
-                return(
-                  <tr>
+        <div className="tableContainer">
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Mobile</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Render data rows */}
+              {dataList[0] ? (
+                dataList.map((el) => (
+                  <tr key={el._id}>
                     <td>{el.name}</td>
                     <td>{el.email}</td>
                     <td>{el.mobile}</td>
                     <td>
-                      <button className='btn btn-edit' onClick={()=>handleEdit(el)}>Edit</button>
-                      <button className='btn btn-delete' onClick={()=>handleDelete(el._id)}>Delete</button>
+                      <button
+                        className="btn btn-edit"
+                        onClick={() => handleEdit(el)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="btn btn-delete"
+                        onClick={() => handleDelete(el._id)}
+                      >
+                        Delete
+                      </button>
                     </td>
                   </tr>
-                )
-              }))
-              : (
-                <p style={{textAlign : "center"}}>No data</p>
-              )
-            }
-          </tbody>
+                ))
+              ) : (
+                <p style={{ textAlign: "center" }}>No data</p>
+              )}
+            </tbody>
           </table>
         </div>
-
-
-
       </div>
     </>
   );
